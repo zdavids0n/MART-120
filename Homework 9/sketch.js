06092076e7ec693ff9a424201ad5143a6c5b7b70
zdavids0n <144194762+zdavids0n@users.noreply.github.com>
2023-10-30 23:58:01 -0600

    Homework 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(120, 45, 78);
+  fill(125,10,160);
+  circle (200,150, 182);
+  fill (111,55,67);
+  rect (12,87,125);
+  fill (22,10,3);
+  elipse (33,56,72,13);
+  fill (10,70,5);
+  triangle (2,33,41,22,7,62);
+  line (22,66,4,12)
+  fill (10,10,10);
+  point (30,20)
+  point (50,20)
+
+}
